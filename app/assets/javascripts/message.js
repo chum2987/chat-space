cf9b086f009d563589b09be4ea9d8b6c8137d576
chum2987 <chum2987@gmai.com>
2020-03-12 19:24:44 +0900

    add incremental search

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -39,6 +39,7 @@ $(function() {
       return html;
     };
    }
+
   $("#new_message").on('submit', function(e) {
     e.preventDefault()
     var formData = new FormData(this);
@@ -62,4 +63,60 @@ $(function() {
     });
     return false;
   });
+
+  
+  // $(function() {
+    
+    // function buildHTML(message){
+
+    //   let image = ( message.image ) ? `<img class="chat-main__message-list__all__messages__image src=${message.image}>`:"";
+      
+    //   let html =
+    //   `<div class="chat-main__message-list__all" data-message-id="${message.id}">
+    //     <div class="chat-main__message-list__all__title">
+    //       <div class="chat-main__message-list__all__title__name">
+    //       ${message.user_name}
+    //       </div>
+    //       <div class="chat-main__message-list__all__title__date">
+    //       ${message.date}
+    //       </div>
+    //     </div>
+    //     <div class="chat-main__message-list__all__messages">
+    //       <p class="chat-main__message-list__all__messages__content">
+    //       ${message.content}
+    //       </p>
+    //       ${image}
+    //     </div>
+    //   </div>`
+    //   return html;
+    // }
+    
+  var reloadMessages = function() {
+  
+    var last_message_id = $(".chat-main__message-list__all:last").data("message-id");
+
+    $.ajax({
+      url: "api/messages",
+      type: "get",
+      dataType: "json",
+      data: {last_id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0){
+        var insertHTML = '';
+        $.each(messages, function(i,message){
+          insertHTML += buildHTML(message);
+        });
+        $(".chat-main__message-list").append(insertHTML);
+        $(".chat-main__message-list").animate({scrollTop: $('.chat-main__message-list')[0].scrollHeight});
+      }
+    })
+    .fail(function() {
+      alert("自動更新エラー");
+    });
+  };
+  // });
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
